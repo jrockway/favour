36d7e4f478d790603620e9f54f3b16a84c3313c6
Jonathan Rockway <jon@jrock.us>
2008-11-09 00:12:47 -0600

    add content-types for static files

--- lib/Favour/Server/Static.pm
+++ lib/Favour/Server/Static.pm
@@ -34,16 +34,48 @@ has 'file' => (
     coerce   => 1,
 );
 
+has 'extension' => (
+    is         => 'ro',
+    isa        => 'Str',
+    lazy_build => 1,
+);
+
+has 'content_type' => (
+    is         => 'ro',
+    isa        => 'Str',
+    lazy_build => 1,
+);
+
 sub BUILD {
     my $self = shift;
     confess $self->file. ' does not exist!' unless -f $self->file;
+}
+
+sub _build_extension {
+    my $self = shift;
+    my $filename = $self->file->basename;
+    $filename =~ /[.]([^.]+)$/ and return $1;
+    return '';
+}
+
+my %ext_map = (
+    css  => 'text/css',
+    js   => 'text/javascript',
+    html => 'application/xhtml+xml',
+    png  => 'image/png',
+);
+
+sub _build_content_type {
+    my $self = shift;
+    return $ext_map{$self->extension} || 'application/octet-stream';
 }
 
 sub serve {
     my ($self, $context, @args) = @_;
     my $data_fh = $self->file->openr;
     return $context->make_response(
-        body => $data_fh,
+        body         => $data_fh,
+        content_type => $self->content_type,
     );
 }
 
